7ecfaabe5e2ee995f2798811f4d57610a48859c2
rkankam <raoul.kankam@gmail.com>
2025-06-23 22:04:08 +0200

    Refactor agent.py to enhance code clarity and maintainability

--- mistral-agent/agent.py
+++ mistral-agent/agent.py
@@ -3,6 +3,7 @@ from google.adk.models.lite_llm import LiteLlm
 from google.adk.tools.mcp_tool import MCPToolset, StdioConnectionParams  
 from mcp import StdioServerParameters  
 import os  
+import requests
 
 
 
@@ -17,6 +18,20 @@ toolset = MCPToolset(
     )
 )
   
+def getOHLCV(pair: str, timeframe: str):
+    # Get OHLCV by Pair Address
+    # You can the timeframe, you can chose between 1s, 1m, 5m, 15m, 30m, 1h, 4h, 1d, 1w, 1M
+    # You can only get the last 10 OHLCV data points
+    # Example: pair = "93tjgwff5Ac5ThyMi8C4WejVVQq4tuMeMuYW1LEYZ7bu"
+    # The API is provided by Moralis
+    url = "https://solana-gateway.moralis.io/token/mainnet/pairs/" + pair + "/ohlcv?timeframe=" + timeframe + "&currency=usd&fromDate=2024-11-25&toDate=2024-11-26&limit=10"
+    headers = {
+        "Accept": "application/json",
+        "X-API-Key": os.environ.get("MORALIS_API_KEY")
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
 root_agent = Agent(  
     model=LiteLlm(  
         model="mistral/mistral-medium-latest",  # ← Paramètre model requis  
@@ -25,5 +40,5 @@ root_agent = Agent(
     name="mistral_agent",  
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge.',
-    tools=[toolset]  
+    tools=[toolset,getOHLCV]  
 )
